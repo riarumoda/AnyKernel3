7f113bdf8bd1047cc45d097afc845873327bf096
riarumoda <justinebudiwijaya@gmail.com>
2026-04-10 21:33:20 +0800

    anykernel: add support for vendor_boot

--- anykernel.sh
+++ anykernel.sh
@@ -11,17 +11,37 @@ supported.patchlevels=
 supported.vendorpatchlevels=
 '; }
 
-# boot shell variables
+# check if device have vendor_boot
+if [ -e /dev/block/bootdevice/by-name/vendor_boot ]; then
+  IS_SLOT_DEVICE=1;
+  VENDOR_BOOT_EXIST=1;
+  ui_print "Device have vendor_boot!";
+else
+  IS_SLOT_DEVICE=0;
+  VENDOR_BOOT_EXIST=0;
+  ui_print "Device do not have vendor_boot!";
+fi
+
+# boot variables
 BLOCK=/dev/block/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+# import ak3 core functions
 . tools/ak3-core.sh;
 
 # boot install
 dump_boot;
-
-# end boot install
 write_boot;
+
+# vendor_boot
+if [ $VENDOR_BOOT_EXIST -eq 1 ]; then
+  # vendor_boot variables
+  BLOCK=/dev/block/bootdevice/by-name/vendor_boot;
+  RAMDISK_COMPRESSION=auto;
+  PATCH_VBMETA_FLAG=auto;
+  # vendor_boot install
+  reset_ak;
+  dump_boot;
+  write_boot;
+fi
