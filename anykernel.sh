94152b11c033b1cefcbd66a48b577cb564798892
riarumoda <justinebudiwijaya@gmail.com>
2026-08-11 09:15:42 +0800

    anykernel: allow lmodroid to use the kernel
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -56,7 +56,21 @@ if [ $IS_ON_RECOVERY -eq 1 ]; then
       mkdir -p /tmp/temp_system;
       mount -t ext4 -o ro /dev/block/mapper/system /tmp/temp_system;
       SYSTEM_VER=$(grep "ro.lineage.build.version" /tmp/temp_system/system/build.prop | cut -d'=' -f2 | cut -d'.' -f1)
-      if [ "$SYSTEM_VER" != "22" && "$SYSTEM_VER" != "23" && "$SYSTEM_VER" != "3" && "$SYSTEM_VER" != "4" ]; then
+      # check if SYSTEM_VER is empty
+      if [ -z "$SYSTEM_VER" ]; then
+        ui_print "- Unable to grab properties of ro.lineage.build.version.";
+        ui_print "- Checking if this is LibreMobileOS...";
+        SYSTEM_VER=$(grep "ro.lmodroid.version" /tmp/temp_system/system/build.prop | cut -d'=' -f2 | cut -d'.' -f1)
+        # check if SYSTEM_VER is empty again
+        if [ -z "$SYSTEM_VER" ]; then
+          ui_print "- Unable to grab properties of ro.lmodroid.version.";
+          ui_print "- Aborting installation to prevent compatibility issues.";
+          umount /mnt/temp_system;
+          $BIN/lptools_static unmap system;
+          exit 1;
+        fi
+      fi
+      if [ "$SYSTEM_VER" != "22" && "$SYSTEM_VER" != "23" && "$SYSTEM_VER" != "3" && "$SYSTEM_VER" != "4" && "$SYSTEM_VER" != "5" && "$SYSTEM_VER" != "6" ]; then
         ui_print "- This OS is not supported.";
         ui_print "- Aborting installation to prevent compatibility issues.";
         umount /mnt/temp_system;
@@ -67,7 +81,20 @@ if [ $IS_ON_RECOVERY -eq 1 ]; then
       mkdir -p /tmp/temp_system;
       mount -t ext4 -o ro /dev/block/bootdevice/by-name/system /tmp/temp_system;
       SYSTEM_VER=$(grep "ro.lineage.build.version" /tmp/temp_system/system/build.prop | cut -d'=' -f2 | cut -d'.' -f1)
-      if [ "$SYSTEM_VER" != "22" && "$SYSTEM_VER" != "23" && "$SYSTEM_VER" != "3" && "$SYSTEM_VER" != "4" ]; then
+      # check if SYSTEM_VER is empty
+      if [ -z "$SYSTEM_VER" ]; then
+        ui_print "- Unable to grab properties of ro.lineage.build.version.";
+        ui_print "- Checking if this is LibreMobileOS...";
+        SYSTEM_VER=$(grep "ro.lmodroid.version" /tmp/temp_system/system/build.prop | cut -d'=' -f2 | cut -d'.' -f1)
+        # check if SYSTEM_VER is empty again
+        if [ -z "$SYSTEM_VER" ]; then
+          ui_print "- Unable to grab properties of ro.lmodroid.version.";
+          ui_print "- Aborting installation to prevent compatibility issues.";
+          umount /mnt/temp_system;
+          exit 1;
+        fi
+      fi
+      if [ "$SYSTEM_VER" != "22" && "$SYSTEM_VER" != "23" && "$SYSTEM_VER" != "3" && "$SYSTEM_VER" != "4" && "$SYSTEM_VER" != "5" && "$SYSTEM_VER" != "6" ]; then
         ui_print "- This OS is not supported.";
         ui_print "- Aborting installation to prevent compatibility issues.";
         umount /mnt/temp_system;
@@ -78,7 +105,19 @@ if [ $IS_ON_RECOVERY -eq 1 ]; then
 elif [ $IS_ON_RECOVERY -eq 0 ]; then
   if [ $KERNEL_BUILD_TYPE == "Weekly" ];then
     SYSTEM_VER=$(getprop ro.lineage.build.version | cut -d'.' -f1);
-    if [ "$SYSTEM_VER" != "22" && "$SYSTEM_VER" != "23" && "$SYSTEM_VER" != "3" && "$SYSTEM_VER" != "4" ]; then
+    # check if SYSTEM_VER is empty
+      if [ -z "$SYSTEM_VER" ]; then
+        ui_print "- Unable to grab properties of ro.lineage.build.version.";
+        ui_print "- Checking if this is LibreMobileOS...";
+        SYSTEM_VER=$(getprop ro.lmodroid.version | cut -d'.' -f1)
+        # check if SYSTEM_VER is empty again
+        if [ -z "$SYSTEM_VER" ]; then
+          ui_print "- Unable to grab properties of ro.lmodroid.version.";
+          ui_print "- Aborting installation to prevent compatibility issues.";
+          exit 1;
+        fi
+      fi
+    if [ "$SYSTEM_VER" != "22" && "$SYSTEM_VER" != "23" && "$SYSTEM_VER" != "3" && "$SYSTEM_VER" != "4" && "$SYSTEM_VER" != "5" && "$SYSTEM_VER" != "6" ]; then
       ui_print "- This OS is not supported.";
       ui_print "- Aborting installation to prevent compatibility issues.";
       exit 1;
@@ -92,6 +131,8 @@ if [ $KERNEL_BUILD_TYPE == "Weekly" ]; then
     ui_print "- LineageOS Version: $SYSTEM_VER";
   elif [$SYSTEM_VER == "3" || $SYSTEM_VER == "4" ]; then
     ui_print "- /e/ OS Version: $SYSTEM_VER";
+  elif [$SYSTEM_VER == "5" || $SYSTEM_VER == "6" ]; then
+    ui_print "- LibreMobileOS Version: $SYSTEM_VER";
   fi
 fi
 ui_print "- Kernel Build Type: $KERNEL_BUILD_TYPE";
